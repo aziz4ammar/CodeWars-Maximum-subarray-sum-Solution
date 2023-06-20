27a414cb269cfcfb391cac5aa58a520ecfff9d2e
Mohamed Aziz Ammar <108346635+aziz4ammar@users.noreply.github.com>
2023-06-20 21:21:33 +0100

    Final Commit

---
+++ Maximum subarray sum.cs
@@ -0,0 +1,21 @@
+using System;
+
+public static class Kata
+{
+    public static int MaxSequence(int[] arr)
+    {
+        if (arr.Length == 0)
+            return 0;
+
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            currentSum = Math.Max(arr[i], currentSum + arr[i]);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
